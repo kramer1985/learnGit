fe30fb4a6727f17617678949da832c29a716ab63
necib yassine <necib.yassine.tux@gmail.com>
2018-06-18 10:10:20 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int a = 20;
+int adresse = 1234;
+int valeur = 400;
+
+int send_data(int entier, int entier2)
+{
+ int result = 0;
+ result = entier + entier2;
+ return result;
+}
+
+int main ()
+{
+char* ptr= "bonjour";
+char* tabptr[]={"test", "coucou", "informatique", "linux"};
+
+
+	send_data(adresse, valeur);
+	printf("valeur : %d\n", a);
+	printf("taille de ptr : %d\n", strlen(ptr));
+	printf("taille de tabptr : %d\n", sizeof(tabptr));
+
+return 0 ;
+}
