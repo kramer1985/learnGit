1ad715bdda3b0c5c2e30d6c990c3661242982328
necib yassine <necib.yassine.tux@gmail.com>
2018-06-18 11:12:53 +0200

    another line
    Signed-off-by: necib yassine <necib.yassine.tux@gmail.com>

--- main.c
+++ main.c
@@ -18,7 +18,8 @@ char* ptr= "bonjour";
 char* tabptr[]={"test", "coucou", "informatique", "linux"};
 
 printf("this a code to learn git :)\n");
-printf("We want to create a patch and apply it to another branch :) )\n");
+printf("We want to create a patch and apply it to another branch :) \n");
+printf("We also want to squash if needed \n");
 	send_data(adresse, valeur);
 	printf("valeur : %d\n", send_data(adresse, valeur));
 	printf("valeur : %d\n", a);
