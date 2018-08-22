ee4bfb2fbfe010c7297ef4012d0be3f93c2d15e7
necib yassine <necib.yassine.tux@gmail.com>
2018-08-22 14:12:25 +0200

    add some files

---
+++ proto.c
@@ -0,0 +1,10 @@
+#include "proto.h"
+
+/*fonction*/
+unsigned int send_data(int entier, int entier2)
+{
+ int result = 0;
+ result = entier + entier2;
+ return result;
+}
+
---
+++ proto.h
@@ -0,0 +1,7 @@
+#ifndef PROTO_H
+#define PROTO_H
+
+
+unsigned int send_data(int entier, int entier2);
+
+#endif
